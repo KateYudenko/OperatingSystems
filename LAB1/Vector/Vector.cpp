7edf6590966f54a79c537262c1fad5bf935ad3e3
KateYudenko <99791433+KateYudenko@users.noreply.github.com>
2022-03-08 21:43:14 +0300

    Add files via upload

---
+++ LAB1/Vector/Vector.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "Vector.h"
+
+Number Vector::radius() {
+	return Number::sqrt(x * x + y * y);
+}
+
+Number Vector::angle() {
+	Number r = Vector::radius();
+	if (r.value != 0)
+		return Number::asin(x / r);
+	else
+		return ZERO_NUMBER;
+}
+
+Vector Vector::add(Vector v1, Vector v2) {
+	return Vector(v1.x + v2.x, v1.y + v2.y);
+}
+
+Vector Vector::subtract(Vector v1, Vector v2) {
+	return Vector(v1.x - v2.x, v1.y - v2.y);
+}
